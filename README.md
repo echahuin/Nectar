2e72a6819627019c8769081dc7cfc34d14a03711
echahuin <edgachahuinauccafuro@est.frba.utn.edu.ar>
2020-04-10 00:02:18 -0300

    agregamos emailjs

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Nectar
 Pagina de aterrizaje para venta de productos de hotmart 
+Tareas a relalizar:
+
+agregar precio de los cursos
+Agregar imagenes png con detalle de comprasegura.. etc.
+agregar una tarjet con el detalle del producto que se esta ofreciendo
+agregar enlace a todas las redes sociales.
+
--- js/main.js
+++ js/main.js
@@ -12,33 +12,59 @@ $(document).scroll(function(){
         s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
         var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
     })();
-    function myAlertFunction() {
-      alert("You selected some text!");
-    }
-    function vermas(id){
-    if(id==="mas"){
-    document.getElementById("desplegar").style.display="block";   
-    document.getElementById("mas").style.display="none"; 
-    }
-    else{
-    document.getElementById("desplegar").style.display="none";
-    document.getElementById("mas").style.display="inline";
-    }
-    }
-    $(document).ready(function(){
+    
+    (function(){
+      emailjs.init("user_Tig0bFGN8WnM1kWzoJWWw");
+   })();
 
-      var height = $(window).height();
+    // function myAlertFunction() {
+    //   alert("You selected some text!");
+    // }
 
-      $('#div2').height(height);
-});
+    // function vermas(id){
+    // if(id==="mas"){
+    // document.getElementById("desplegar").style.display="block";   
+    // document.getElementById("mas").style.display="none"; 
+    // }
+    // else{
+    // document.getElementById("desplegar").style.display="none";
+    // document.getElementById("mas").style.display="inline";
+    // }
+//     // }
+//     $(document).ready(function(){
+
+//       var height = $(window).height();
+
+//       $('#div2').height(height);
+// });
 
   var app = new Vue({
     el: '#app',
     data: {
       message: 'Hello Vue!',
+      from_name: '',
+      from_email: '',
     },
     methods: {
-    
+      //
+      enviar(){
+        let data = {
+            from_name: this.from_name,
+            from_email: this.from_email
+        };
+        
+        emailjs.send("gmail","template_Yv5uIFz4", data)
+        .then(function(response) {
+            if(response.text === 'OK'){
+                alert('El correo se ha enviado de forma exitosa');
+            }
+           console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+        }, function(err) {
+            alert('Ocurrió un problema al enviar el correo');
+           console.log("FAILED. error=", err);
+        });
+      } 
+      //
     },
     computed: {
     },
